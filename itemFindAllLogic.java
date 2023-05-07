e34f2bf0cffa8535c73520931c3c920c667c035c
Syouchan <81793181+Syouchan@users.noreply.github.com>
2023-05-07 16:59:26 +0900

    Add files via upload

--- itemFindAllLogic.java
+++ itemFindAllLogic.java
@@ -1,34 +1,34 @@
 package javasys.cake.logic;
 
+import javasys.cake.common.CakeBusinessException;
+import javasys.cake.common.CakeSystemException;
+import javasys.cake.entity.Item;
+import javasys.cake.dao.ConnectionManager;
+import javasys.cake.dao.ItemDAO;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import javasys.cake.common.CakeBusinessException;
-import javasys.cake.common.CakeSystemException;
-import javasys.cake.dao.ConnectionManager;
-import javasys.cake.dao.ItemDAO;
-import javasys.cake.entity.Item;
-
 public class ItemFindAllLogic {
 
     public ArrayList<Item> findAllItem(String itemCode) throws CakeBusinessException {
-
         Connection con = null;
-        ArrayList<Item> itemList = new ArrayList<Item>();
+        ArrayList<Item> itemList = null;
+        Item item = null;
 
         try {
-            //データベースの接続を取得する
+            // データベースの接続を取得する
             con = ConnectionManager.getConnection();
             ItemDAO itemDAO = new ItemDAO(con);
-            itemList = itemDAO.findAllItemByCode(itemCode);
+            item = itemDAO.findAllItemByCode(itemCode);
 
-            //検索結果がない場合、業務エラーを発生させる
-            if (itemList == null || itemList.size() == 0) {
+            // 検索結果がない場合、業務エラーを発生させる
+            if (item == null) {
                 throw new CakeBusinessException("商品リストが存在しません");
             }
         } catch (SQLException e) {
-            //データベースエラーの場合、システムエラー発生させる
+            // データベースエラーの場合、システムエラー発生させる
             throw new CakeSystemException("システムエラーが発生しました。管理者に連絡してください", e);
         } finally {
             try {
